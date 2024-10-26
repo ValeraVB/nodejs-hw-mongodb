6e97daff0df09689823263a1232aa3c3e7afad36
Valeriy <vb@profish.com.ua>
2024-10-26 18:42:34 +0300

    add node.js home work 02

---
+++ src/server.js
@@ -0,0 +1,63 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+
+import { env } from './utils/env.js';
+
+import * as contactServices from './services/contacts.js';
+
+export const startServer = () => {
+  const app = express();
+
+  app.use(cors());
+  const logger = pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  });
+  app.use(logger);
+
+  app.get('/contacts', async (req, res) => {
+    const data = await contactServices.getContacts();
+
+    res.json({
+      status: 200,
+      message: 'Successfully find contacts',
+      data,
+    });
+  });
+
+  app.get('/contacts/:id', async (req, res) => {
+    const { id } = req.params;
+    const data = await contactServices.getContactById(id);
+
+    if (!data) {
+      return res.status(404).json({
+        status: 404,
+        message: `Contact with id=${id} not found`,
+      });
+    }
+
+    res.json({
+      status: 200,
+      message: `Contact successfully find`,
+      data,
+    });
+  });
+
+  app.use((req, res) => {
+    res.status(404).json({
+      message: `${req.url} not found`,
+    });
+  });
+
+  app.use((error, req, res, next) => {
+    res.status(500).json({
+      message: error.message,
+    });
+  });
+
+  const port = Number(env('PORT', 3000));
+
+  app.listen(port, () => console.log(`Server running on ${port} PORT`));
+};
