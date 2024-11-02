f6158b8995b66d93bcdddb0294e3a5a319fbde64
Valeriy <vb@profish.com.ua>
2024-11-03 01:04:24 +0200

    Add node.js home work 03

---
+++ src/routers/contacts.js
@@ -0,0 +1,32 @@
+import { Router } from 'express';
+
+import * as contactControllers from '../controllers/contacts.js';
+
+import ctrlWrapper from '../utils/ctrlWrapper.js';
+
+const contactsRouter = Router();
+
+contactsRouter.get('/', ctrlWrapper(contactControllers.getContactsController));
+
+contactsRouter.get(
+  '/:id',
+  ctrlWrapper(contactControllers.getContactByIdController),
+);
+
+contactsRouter.post('/', ctrlWrapper(contactControllers.addContactController));
+
+contactsRouter.put(
+  '/:id',
+  ctrlWrapper(contactControllers.upsertContactController),
+);
+
+contactsRouter.patch(
+  '/:id',
+  ctrlWrapper(contactControllers.patchContactController),
+);
+
+contactsRouter.delete(
+  '/:id',
+  ctrlWrapper(contactControllers.deleteContactController),
+);
+export default contactsRouter;
